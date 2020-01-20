4cb18d56be02ffada947ec5d1bfd723c1b43f8c5
Natan <59052957+Natanius18@users.noreply.github.com>
2020-01-20 20:41:58 +0200

    small improving

--- src/main/java/Phonebook.java
+++ src/main/java/Phonebook.java
@@ -3,56 +3,54 @@ import java.util.ArrayList;
 public class Phonebook {
     private static ArrayList<Contact> contacts = new ArrayList<>();
 
-
-    public static ArrayList<Contact> getAllContact(){
+    public static ArrayList<Contact> getAllContact() {
         return contacts;
     }
 
-    public void add(Contact contact){
+    public void add(Contact contact) {
         contacts.add(contact);
     }
 
-    public Contact get(int id){
+    public Contact get(int id) {
         return contacts.get(--id);
     }
 
-    public void del(int id){
+    public void del(int id) {
         if (contacts.size() < id) {
             System.out.println("The contact with index " + id + " doesn't exist!");
-        }else {
+        } else {
             contacts.remove(--id);
             System.out.println("The contact was deleted successfully!");
         }
     }
 
-    public ArrayList<Contact> findByName(String name){
+    public ArrayList<Contact> findByName(String name) {
         ArrayList<Contact> result = new ArrayList<>();
         for (Contact contact : contacts) {
-            if (contact.getName().equals(String.valueOf(name.charAt(0)).toUpperCase()+ name.substring(1))){
+            if (contact.getName().equals(String.valueOf(name.charAt(0)).toUpperCase() + name.substring(1))) {
                 result.add(contact);
             }
         }
         return result;
     }
 
-    public ArrayList<Contact> findByNameStart(String value){
+    public ArrayList<Contact> findByNameStart(String value) {
         ArrayList<Contact> result = new ArrayList<>();
-        for (Contact contact : contacts){
-            if (contact.getName().startsWith(String.valueOf(value.charAt(0)).toUpperCase()+ value.substring(1))){
+        for (Contact contact : contacts) {
+            if (contact.getName().startsWith(String.valueOf(value.charAt(0)).toUpperCase() + value.substring(1))) {
                 result.add(contact);
             }
         }
         return result;
     }
 
-    public ArrayList<Contact> findByPhone(String value){
+    public ArrayList<Contact> findByPhone(String value) {
         ArrayList<Contact> result = new ArrayList<>();
-        for (Contact contact : contacts){
-            if (contact.getPhone().endsWith(value)){
+        for (Contact contact : contacts) {
+            if (contact.getPhone().endsWith(value)) {
                 result.add(contact);
             }
         }
         return result;
     }
-
 }
