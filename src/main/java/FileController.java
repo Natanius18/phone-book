0b0b048e2d694d39615715bbd010677e5061a72f
Natan <59052957+Natanius18@users.noreply.github.com>
2020-01-24 15:11:47 +0200

    big bugs fixed

--- src/main/java/FileController.java
+++ src/main/java/FileController.java
@@ -3,8 +3,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-    public class FileController {
+public class FileController {
         public static void saveToFile(ArrayList<Contact> contacts){
             try {
                 FileWriter fstream = new FileWriter("PhoneBook.txt");
@@ -39,7 +40,11 @@ import java.util.ArrayList;
             String [] data = line.split(";");
             Contact contact = null;
             if (data.length == 4){
-                contact = new Contact(data[0], data[1], data[2], data[3]);
+                String[] dataName = data[0].split(" ");
+                String[] dataSurname = data[1].split(" ");
+                String[] dataNumber = data[2].split(" ");
+                String[] dataAge = data[3].split(" ");
+                contact = new Contact(dataName[1], dataSurname[2], dataNumber[2], dataAge[2]);
             }
             return contact;
         }
